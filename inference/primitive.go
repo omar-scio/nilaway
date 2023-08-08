c10a6c228828aa15532b2b1074d824d760cba9a2
sonalmahajan15 <smahajan@uber.com>
2023-08-08 11:00:49 -0700

    updated error text and want of test cases

--- inference/primitive.go
+++ inference/primitive.go
@@ -40,6 +40,7 @@ type primitiveFullTrigger struct {
 
 func fullTriggerAsPrimitive(pass *analysis.Pass, trigger annotation.FullTrigger) primitiveFullTrigger {
 	producer, consumer := trigger.Prestrings(pass)
+
 	return primitiveFullTrigger{
 		ProducerRepr: producer,
 		ConsumerRepr: consumer,
